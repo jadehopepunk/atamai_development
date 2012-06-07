f3967431fe2913fcc2c286f94a70e52097647563
Craig Ambrose <craig@craigambrose.com>
2012-06-07 17:12:16 +1200

    Added refinerycms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtamaiDevelopment::Application.config.secret_token = '2da32a78ddaaa484e358c12ce07d494ef556c509c4d665df3cbf62f6198279adcd0c805d6a79d9fc5b45ae840af11bef8e556bf741c157a4a7294b80907f4559'
