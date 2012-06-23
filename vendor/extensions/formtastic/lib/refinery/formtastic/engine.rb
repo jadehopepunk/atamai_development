0420fd41eef49329d93328ed757bbe2cd3d07759
Craig Ambrose <craig@craigambrose.com>
2012-06-23 21:53:52 +1200

    Created page tab helper

--- vendor/extensions/formtastic/lib/refinery/formtastic/engine.rb
+++ vendor/extensions/formtastic/lib/refinery/formtastic/engine.rb
@@ -6,6 +6,8 @@ module Refinery
       config.after_initialize do
         Refinery::Core.config.register_stylesheet("formtastic")
         Refinery::Core.config.register_stylesheet("refinery/formtastic/admin")
+        require 'refinery/formtastic/extensions/page_tab'
+        require 'refinery/formtastic/page_tabs_helper'
       end
     end
   end
