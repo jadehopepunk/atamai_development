242a41821b6676bc05c72051890794f6a925b90b
Craig Ambrose <craig@craigambrose.com>
2012-07-22 14:15:18 +1200

    updated theme

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,9 @@ module AtamaiDevelopment
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.to_prepare do
+      Refinery.searchable_models = [Refinery::Page]
+    end    
   end
 end
