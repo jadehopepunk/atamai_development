ef9233489b85ed6565f72001f74dc9190596f642
Craig Ambrose <craig@craigambrose.com>
2012-07-22 13:04:37 +1200

    removed preview password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :preview_password if Rails.env.production?
-
-  private
-
-    def preview_password
-      authenticate_or_request_with_http_basic('Site under development') do |username, password|
-        username == 'preview' && password == 'preview'
-      end
-    end
 end
